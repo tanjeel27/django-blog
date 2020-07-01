dd64ea1c9814284badadc7c22f7ed1335336b9e9
tanjeel27 <tanjeel27@gmail.com>
2020-07-01 14:55:36 +0600

    Second Commit

--- users/models.py
+++ users/models.py
@@ -6,13 +6,12 @@ from PIL import Image
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
-    from PIL import Image
 
     def __str__(self):
         return f'{self.user.username} Profile' 
 
-    def save(self):
-    	super().save()
+    def save(self, *args, **kwargs):
+    	super().save(*args, **kwargs)
 
     	img=Image.open(self.image.path)
 
