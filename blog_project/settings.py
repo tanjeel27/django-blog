498c6cfdd78337e49f5b987ec601bde41b086c2f
tanjeel27 <tanjeel27@gmail.com>
2020-07-02 02:31:17 +0600

    Changed Settings

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -123,6 +123,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
